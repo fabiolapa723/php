335b35d426d998a15feed2d64948005227da9a0b
Fábio Lapa <fabiolapa@192.168.1.4>
2023-03-22 17:09:36 -0300

    Changed index page contents novo.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World from FabioLapa -----! php version is " . PHP_VERSION . "\n";
-print "A change is in the air!\n";
+print "Hello, World from FabioLapa -----! php version is " . PHP_VERSION . "\n";
 ?>
